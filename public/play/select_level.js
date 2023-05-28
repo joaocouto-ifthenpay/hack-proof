dc13ed1224c8cdf711cb195b7fd9ba37c2b383d2
Rui Martins <ispg2019103944@ispgaya.pt>
2023-05-28 18:10:39 +0100

    fix maps js

--- public/play/select_level.js
+++ public/play/select_level.js
@@ -2,10 +2,23 @@ document.addEventListener('DOMContentLoaded', function() {
         var params = new URLSearchParams(window.location.search);
         var scriptElement = document.createElement('script');
         let level = params.get('map');
+        let fileUrl = 'cards_' + level + '.js';
 
-        if (level || existFile('cards_' + level + '.js')) {
-                scriptElement.src = 'cards_' + level + '.js';
-                document.querySelector('.level').textContent = "Nível " + level;
+        if (level) {
+                var request = new XMLHttpRequest();
+                request.open('HEAD', fileUrl, true);
+                
+                request.onreadystatechange = function() {
+                        if (request.readyState === 4) {
+                                if (request.status === 200) {
+                                        scriptElement.src = 'cards_' + level + '.js';
+                                        document.querySelector('.level').textContent = "Nível " + level;
+                                } else {
+                                        window.location.href = 'https://hack-proof-ispgaya.web.app';
+                                }
+                        }
+                };
+                request.send();
         } else {
                 window.location.href = 'https://hack-proof-ispgaya.web.app';
         }
@@ -13,21 +26,23 @@ document.addEventListener('DOMContentLoaded', function() {
 
         // Adiciona o elemento ao elemento pai desejado no DOM, por exemplo, o body
         document.body.appendChild(scriptElement);
+
+
+
+        function existFile(fileUrl) {
+                var request = new XMLHttpRequest();
+                request.open('HEAD', fileUrl, true);
+                
+                request.onreadystatechange = function() {
+                        if (request.readyState === 4) {
+                                if (request.status === 200) {
+                                        return true;
+                                } else {
+                                        return false;
+                                }
+                        }
+                };
+                request.send();
+        }
 });
 
-function existFile(fileUrl) {
-        var request = new XMLHttpRequest();
-        request.open('HEAD', fileUrl, true);
-        
-        request.onreadystatechange = function() {
-                if (request.readyState === 4) {
-                        if (request.status === 200) {
-                                console.log('O arquivo existe.');
-                        } else {
-                                window.location.href = 'https://hack-proof-ispgaya.web.app';
-                        }
-                }
-        };
-
-        request.send();
-}
